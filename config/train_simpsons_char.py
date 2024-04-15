40945ab732db304b851a97a073f2243739a72005
devinbresser <66394890+devinbresser@users.noreply.github.com>
2024-04-14 19:26:02 -0500

    Update train_simpsons_char.py

--- config/train_simpsons_char.py
+++ config/train_simpsons_char.py
@@ -30,7 +30,7 @@ lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 50 # not super necessary potentially
+warmup_iters = 100 # not super necessary potentially
 
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch'
