173a4fc412cfd2e10a83eec58015ae59b6f4fe85
devinbresser <66394890+devinbresser@users.noreply.github.com>
2024-04-14 19:21:45 -0500

    Update train_simpsons_char.py

--- config/train_simpsons_char.py
+++ config/train_simpsons_char.py
@@ -16,21 +16,21 @@ wandb_run_name = 'nano-gpt'
 dataset = 'simpsons'
 gradient_accumulation_steps = 1
 batch_size = 64
-block_size = 256 # context of up to 256 previous characters
+block_size = 20 # context of up to 256 previous characters
 
 # baby GPT model :)
 n_layer = 6
 n_head = 6
-n_embd = 384
+n_embd = 32
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 500
+lr_decay_iters = 500 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 100 # not super necessary potentially
+warmup_iters = 50 # not super necessary potentially
 
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch'
